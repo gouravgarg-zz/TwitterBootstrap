2337528029136db86607ce6a74ae1da19b87d3ba
gouravgarg <gouravgargg@gmail.com>
2017-02-02 15:38:26 +0530

    first checkin
    bootstrap + angular + angular routing + single page application demo

---
+++ sp.js
@@ -0,0 +1,20 @@
+angular.module('SinglePageApp',['ngRoute']) 
+
+.config(function($routeProvider){
+		$routeProvider
+		.when('/home', {
+			templateUrl: 'home.html'
+			// ajax binding
+		})
+		.when('/about', {
+			templateUrl: 'about.html'
+			// in line binding
+		})
+		.when('/contact', {
+			templateUrl: 'contact.html'
+			// in line binding
+		})
+		.otherwise({
+			redirectTo: '/home'
+		})
+})
