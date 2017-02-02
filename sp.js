5141ea4d144d73491ac6690a57c7cf3d9689c2ac
gouravgarg <gouravgargg@gmail.com>
2017-02-02 16:51:24 +0530

    Dynamic data adding
    from http or from any other resources

--- sp.js
+++ sp.js
@@ -14,6 +14,11 @@ angular.module('SinglePageApp',['ngRoute'])
 			templateUrl: 'contact.html'
 			// in line binding
 		})
+		.when('/movies', {
+			templateUrl: 'movies.html',
+			controller : 'moviesController'
+			// in line binding
+		})
 		.otherwise({
 			redirectTo: '/home'
 		})
@@ -25,3 +30,25 @@ angular.module('SinglePageApp',['ngRoute'])
     };
 
 })
+
+.factory('movieFactory',function($http){
+	return {
+		get : function(){
+			return $http.get('movies.json');
+			// it's called promise call/async call
+		}
+	}
+})
+
+.controller('moviesController',function($scope,movieFactory){
+	movieFactory
+	.get()
+	.then(function(response){
+		console.log(response);
+		$scope.movies=response.data;
+	})
+	.catch(function(error){
+		console.log(error);
+	});
+})
+// promise : rejected/pending/resolved
